b111941bb52964d23c9b617fdf3ff4e7e382c318
Memo Alvarez <memoalvarez713@gmail.com>
2019-06-11 13:42:07 -0500

    [FIX] Se corrigio el tipo de dato

--- crm_module/inherit/crm_lead.py
+++ crm_module/inherit/crm_lead.py
@@ -6,7 +6,7 @@ from odoo import models, api, fields, exceptions
 class PurchaseOrder(models.Model):
 	_inherit = 'crm.lead'
 
-	ingreso_conectividad=fields.float(string="Ingreso conectividad") 
-	ingreso_de_ingenieria=fields.float(string="Ingreso de ingenieria")
+	ingreso_conectividad=fields.Float(string="Ingreso conectividad") 
+	ingreso_de_ingenieria=fields.Float(string="Ingreso de ingenieria")
 
 
