c749058f5139bca66a0df49dd8f4a378f2b32bb6
Memo Alvarez <memoalvarez713@gmail.com>
2019-06-11 13:59:06 -0500

    [FIX] Se corrigieron modulos del modulo de compras

--- compras_module/inherit/purchase_order.py
+++ compras_module/inherit/purchase_order.py
@@ -6,4 +6,4 @@ from odoo import models, api, fields, exceptions
 class PurchaseOrder(models.Model):
 	_inherit = 'purchase.order'
 
-	ficha_tecnica=fields.Many2many("ir.attachment", string "Ficha técnica")
+	ficha_tecnica=fields.Many2many("ir.attachment", string="Ficha técnica")
