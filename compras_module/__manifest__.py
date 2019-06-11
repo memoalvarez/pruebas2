9c94c84ab04fadb216326797edb1cf4b760b9b07
Memo Alvarez <memoalvarez713@gmail.com>
2019-06-11 13:55:48 -0500

    [ADD] Se agregaron cambios al modulo de compras

---
+++ compras_module/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'compras_module',
+    'author': 'Assetel',
+    'website': 'www.assetel.com',
+    'summary' : 'Coded by: Guillermo Ruiz Alvarez',
+    'depends': [
+        'base',
+        'purchase',
+    ],
+    'data': [
+    # VIEWS
+    'views/purchase_order.xml',
+    
+    
+    ],
+    'installable': True,
+    'application': False
+}
---
+++ compras_module/inherit/purchase_order.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields, exceptions
+
+
+class PurchaseOrder(models.Model):
+	_inherit = 'purchase.order'
+
+	ficha_tecnica=fields.Many2many("ir.attachment", string "Ficha técnica")
