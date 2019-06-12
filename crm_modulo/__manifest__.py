2b42a3ce6fc78642830241ca8c03768007fc8c43
Memo Alvarez <memoalvarez713@gmail.com>
2019-06-12 10:37:11 -0500

    [ADD] Agregue campos al crm

---
+++ crm_modulo/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'crm.lead',
+    'author': 'Assetel',
+    'website': 'www.assetel.com',
+    'summary' : 'Coded by: Guillermo Ruiz Alvarez',
+    'depends': [
+        'base',
+        'crm',
+    ],
+    'data': [
+    # VIEWS
+    'views/purchase_order.xml',
+    'views/monto_maximo.xml',
+    'views/purchase_order_menu.xml',
+    
+    ],
+    'installable': True,
+    'application': False
+}
---
+++ crm_modulo/inherit/crm_lead.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields, exceptions
+
+
+class PurchaseOrder(models.Model):
+	_inherit = 'crm.lead'
+
+	Agente_de_compra.fields=float(string=Agende de compra)
+	
+
+
+	
