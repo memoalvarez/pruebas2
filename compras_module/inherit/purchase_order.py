04071686be917dee2b24dfc2e598b6168104a18c
Memo Alvarez <memoalvarez713@gmail.com>
2019-06-12 12:24:34 -0500

    [ADD] Se añadio campo relacional

--- compras_module/inherit/purchase_order.py
+++ compras_module/inherit/purchase_order.py
@@ -7,3 +7,5 @@ class PurchaseOrder(models.Model):
 	_inherit = 'purchase.order'
 
 	ficha_tecnica=fields.Many2many("ir.attachment", string="Ficha técnica")
+
+	usuario_requisicion = fields.Many2one("res.users", string="Usuario requisicion")
