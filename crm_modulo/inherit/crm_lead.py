f43d1105016a21a50432fbcdd50acfd28d435f60
Memo Alvarez <memoalvarez713@gmail.com>
2019-06-12 10:53:27 -0500

    [FIX] Corrección de errores para los campos del CRM

--- crm_modulo/inherit/crm_lead.py
+++ crm_modulo/inherit/crm_lead.py
@@ -4,10 +4,10 @@ from odoo import models, api, fields, exceptions
 
 
 class PurchaseOrder(models.Model):
-	_inherit = 'crm.lead'
+	_inherit = 'crm_modulo'
 
 	Agente_de_compra.fields=float(string=Agende de compra)
-	
+
 
 
 	
