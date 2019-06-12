b530710b0c877f105946ace5b8fa5f0f3fa62db3
Memo Alvarez <memoalvarez713@gmail.com>
2019-06-12 14:03:43 -0500

    [ADD] Agregar seguridad

---
+++ security/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'security',
+    'author': 'Assetel',
+    'website': 'www.assetel.com',
+    'summary' : 'Coded by: Guilermo Ruiz Alvarez',
+    'depends': [
+        'base',
+        
+    ],
+    'data': [
+    # VIEWS
+    'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'application': False
+}
