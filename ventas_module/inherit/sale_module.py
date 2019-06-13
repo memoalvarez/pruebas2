02b636c32d0888391a489ca943a95d383e70763f
Memo Alvarez <memoalvarez713@gmail.com>
2019-06-13 10:31:37 -0500

    [FIX] Corrección 10:31

--- ventas_module/inherit/sale_module.py
+++ ventas_module/inherit/sale_module.py
@@ -4,7 +4,7 @@ from odoo import models, api, fields, exceptions
 
 
 class PurchaseOrder(models.Model):
-	_inherit = 'sale_module'
+	_inherit = 'sale.order'
 
 	agente_de_venta=fields.Char(string="Agente de venta")
 	importe_de_venta=fields.Float(string="Importe de venta")
