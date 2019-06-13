9a926d12593363cf5092918a4b28befc5369b1db
Memo Alvarez <memoalvarez713@gmail.com>
2019-06-13 13:13:25 -0500

    [FIX]2

--- canal_distribucion/inherit/canal_de_distribucion.py
+++ canal_distribucion/inherit/canal_de_distribucion.py
@@ -4,7 +4,7 @@ from odoo import models, api, fields, exceptions
 
 
 class ContactModule(models.Model):
-	_inherit = 'canal.de.distribucion'
+	_inherit = 'res.partner'
 
 	canal_de_distribucion=fields.Many2one("name", string="Canal de distribución")
 	
